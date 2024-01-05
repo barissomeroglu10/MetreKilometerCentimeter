2384339a4fe119cb9a7938dd104b61d82595c412
baris <baris@BARIS_SOMEROGLU>
2024-01-05 16:24:11 +0300

    Add project files.

---
+++ MetreKilometerCantimeter/MetreKilometerCantimeter.cpp
@@ -0,0 +1,33 @@
+﻿/*
+In this code, we get a number for length from the user. Then convert the length from
+metre to kilometer and metre to centimeter.
+
+
+Developer: Barış Someroğlu
+Date: 05.01.2024 / 4.20 p.m.
+*/
+
+#include <iostream>
+
+using namespace std;
+
+int main()
+{
+	double m, km, cm;
+
+	cout << "Please Enter Length: ";
+	cin >> m;
+
+	cout << "\n";
+
+	km = m / 1000;
+	cm = m * 100;
+
+	cout << "km value for the length: " << km << endl;
+
+	cout << "cm value for the length: " << cm;
+
+	cout << "\n";
+
+	return 0;
+}
